38b24754a91617eb4392b85859ade1b25e61b5fd
Danilo Souza Brun <danilo.brun@al.infnet.edu.br>
2022-08-27 20:54:39 -0300

    Protege a rota private e configura autenticação via JWT

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.get('/', (req, res) => {
 })
 
 // Private Route - Only Logged Users
-app.get('/user/:id', async (req, res) => {
+app.get('/user/:id', checkToken, async (req, res) => {
 
     const id = req.params.id
 
@@ -33,6 +33,29 @@ app.get('/user/:id', async (req, res) => {
     res.status(200).json({ user })
 })
 
+function checkToken(req, res, next) {
+
+    //Get token by headers access with array authorization
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(" ")[1]
+
+    if(!token) {
+        res.status(401).json({ msg: 'Acesso negado!' })
+    }
+
+    try {
+
+        const secret = process.env.SECRET
+
+        jwt.verify(token, secret)
+
+        next()
+
+    } catch(error) {
+        res.status(400).json({ msg: 'Token inválido!' })
+    }
+
+}
 // Credencials
 const dbUser = process.env.DB_USER
 const dbPassword = process.env.DB_PASS
