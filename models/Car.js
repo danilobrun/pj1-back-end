f1c16e28518d0d2fa4980bd2f036bbb5eb6a24fa
Danilou Souza Brun <danilobrun@Danilous-MacBook-Pro.local>
2022-08-29 16:52:00 -0300

    cria a cada model para os carros

---
+++ models/Car.js
@@ -0,0 +1,15 @@
+// IMPORTS
+const mongoose = require('mongoose')
+
+// Acesssos e proriedades dessa classe
+const Car = mongoose.model('Car', {
+    name: String,
+    brand: String,
+    model: String,
+    year: String,
+    transmission: String,
+    engine: String,
+    color: String,
+    door: String
+
+})
