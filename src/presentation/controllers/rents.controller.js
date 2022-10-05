248777c17aebfdbbe1b61708c103be1c3735329b
Danilo Souza Brun <danilo.brun@al.infnet.edu.br>
2022-10-05 20:38:27 -0300

    cria rota de algueis rentsGet e controller e validations

--- src/presentation/controllers/rents.controller.js
+++ src/presentation/controllers/rents.controller.js
@@ -1,16 +1,17 @@
 const Car = require("../../../models/Car");
 const Rent = require("../../../models/Rent");
+const mongoose = require('mongoose')
 
 
 const rentCar = async (req, res) => {
-    
+
     // Get query params id {car_id} 
     const { id } = req.params
     console.log(id);
 
     // Validations
-    if(!id) {
-        return res.status(422).json({ msg: 'Favor informar o id do carro!'})
+    if (!id) {
+        return res.status(422).json({ msg: 'Favor informar o id do carro!' })
     }
 
     // check if car exists
@@ -32,9 +33,9 @@ const rentCar = async (req, res) => {
         rent.save()
         return res.status(200).send(`O carro selecionado foi 
         ${name} ${brand} ${model} ${license_plate}`)
-        
+
     } else {
-        return res.status(404).json({ msg: 'Carro não encontrado!'})
+        return res.status(404).json({ msg: 'Carro não encontrado!' })
     }
 
 
@@ -43,7 +44,7 @@ const rentCar = async (req, res) => {
     //     await car.save()
 
     //     res.status(201).json({ msg: 'Carro registrado na base com sucesso!'})
-        
+
     // } catch (error) {
     //     console.log('error', error);
 
@@ -55,7 +56,68 @@ const rentCar = async (req, res) => {
     // }
 }
 
+const rentsGet = async (req, res) => {
+    // Recive id rent with param
+    const { id } = req.params
+
+    // Validations
+    if(!id) {
+        return res.status(422).json({ msg: 'Favor informar o aluguel' })
+    }
+
+    //check if rent exists
+    const rentExists = await Rent.findOne({ _id: id})
+
+    if (!rentExists) {
+        return res.status(422).json({ msg: 'Aluguel não existe' })
+    }
+
+    // get rent
+    try {
+        // aggregate database
+        const result= await Rent.aggregate(
+            [
+                {
+                    '$addFields': {
+                        '_idCar': {
+                            '$toObjectId': '$car_id'
+                        },
+                        '_idUser': {
+                            '$toObjectId': '$user_id'
+                        }
+                    }
+                }, {
+                    '$lookup': {
+                        'from': 'cars',
+                        'localField': '_idCar',
+                        'foreignField': '_id',
+                        'as': 'car_selected'
+                    }
+                }, {
+                    '$lookup': {
+                        'from': 'users',
+                        'localField': '_idUser',
+                        'foreignField': '_id',
+                        'as': 'user'
+                    }
+                },
+                {
+                    '$match': {
+                        '_id': new mongoose.Types.ObjectId(id)
+                    }
+                }
+            ]
+        )
+        console.log(result);
+        return res.status(200).send(result)
+    } catch (error) {
+        console.log('error', error);
+        res.status(500).json({ msg: 'Aconteceu um erro no servidor, tente novamente mais tarde!' })
+    }
+}
+
 
 module.exports = {
-    rentCar
+    rentCar,
+    rentsGet
 }
--- src/routes/rent.cars.routes.js
+++ src/routes/rent.cars.routes.js
@@ -1,10 +1,11 @@
 const { checkToken } = require('../middleware/checktoken')
-const { rentCar } = require('../presentation/controllers/rents.controller')
+const { rentCar, rentsGet } = require('../presentation/controllers/rents.controller')
 
 const rentsRoutes = (app) => {
 
     app.post('/rentcar/:id', checkToken, rentCar)
     app.get('/rentscars/:id', checkToken, rentCar)
+    app.get('/rents/:id', checkToken, rentsGet)
 }
 
 
